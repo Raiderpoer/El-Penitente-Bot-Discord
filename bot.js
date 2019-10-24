2a034bf3c0be8269462726dd4a9e60a7ff7f11b1
Germán Flores <gfloresmarin@gmail.com>
2019-10-24 17:25:55 +0200

    Lanzamiento Inicial
    Lanzamiento inicial del BOT

---
+++ bot.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("El Penitente listo!");
+
+    client.user.setPresence( {
+        status: "online",
+        game: {
+            name: "Encendiendo el cirio",
+            type: "PLAYING"
+        }
+    });
+ });
+client.on('guildMemberAdd', member => {
+        let memberTag = member.user.tag;
+    member.guild.channels.get('636699247626027011').send("":bell: :hammer:" Hola <@" + member.id + "> bienvenid@ al servidor de Discord de **El Penitente**!");
+});
+client.login(process.env.BOT_TOKEN); 
